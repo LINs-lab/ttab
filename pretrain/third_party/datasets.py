246d998ae478724db302f680bd21d6f3fc1ee2e8
MarcellusZhao <zhaohao1202@outlook.com>
2023-08-07 01:00:40 +0200

    Provide an improved pretraining script as requested by #4

--- pretrain/third_party/datasets.py
+++ pretrain/third_party/datasets.py
@@ -1,7 +1,97 @@
 # A collection of datasets class used in pretraining models.
 
 # imports.
+import os
+import functools
 import torch
+import sys
+sys.path.append("..")
+
+from ttab.loads.datasets.dataset_shifts import NoShiftedData, SyntheticShiftedData
+from ttab.loads.datasets.mnist import ColoredSyntheticShift
+from ttab.loads.datasets.loaders import BaseLoader
+from ttab.loads.datasets.datasets import OfficeHomeDataset, PACSDataset, CIFARDataset, WBirdsDataset, ColoredMNIST
+
+def get_train_dataset(config) -> BaseLoader:
+    """Get the training dataset from `config`."""
+    data_shift_class = functools.partial(NoShiftedData, data_name=config.data_name)
+    if "cifar" in config.data_name:
+        train_dataset = CIFARDataset(
+            root=os.path.join(config.data_path, config.data_name),
+            data_name=config.data_name,
+            split="train",
+            device=config.device,
+            data_augment=True,
+            data_shift_class=data_shift_class,
+        )
+        val_dataset = CIFARDataset(
+            root=os.path.join(config.data_path, config.data_name),
+            data_name=config.data_name,
+            split="test",
+            device=config.device,
+            data_augment=False,
+            data_shift_class=data_shift_class,
+        )
+    elif "officehome" in config.data_name:
+        _data_names = config.data_name.split("_")
+        dataset = OfficeHomeDataset(
+            root=os.path.join(config.data_path, _data_names[0], _data_names[1]),
+            device=config.device,
+            data_augment=True,
+            data_shift_class=data_shift_class,
+        ).split_data(fractions=[0.9, 0.1], augment=[True, False], seed=config.seed)
+        train_dataset, val_dataset = dataset[0], dataset[1]
+    elif "pacs" in config.data_name:
+        _data_names = config.data_name.split("_")
+        dataset = PACSDataset(
+            root=os.path.join(config.data_path, _data_names[0], _data_names[1]),
+            device=config.device,
+            data_augment=True,
+            data_shift_class=data_shift_class,
+        ).split_data(fractions=[0.9, 0.1], augment=[True, False], seed=config.seed)
+        train_dataset, val_dataset = dataset[0], dataset[1]
+    elif config.data_name == "waterbirds":
+        train_dataset = WBirdsDataset(
+            root=os.path.join(config.data_path, config.data_name),
+            split="train",
+            device=config.device,
+            data_augment=True,
+            data_shift_class=data_shift_class,
+        )
+        val_dataset = WBirdsDataset(
+            root=os.path.join(config.data_path, config.data_name),
+            split="val",
+            device=config.device,
+            data_augment=False,
+        )
+    elif config.data_name == "coloredmnist":
+        data_shift_class = functools.partial(
+            SyntheticShiftedData,
+            data_name=config.data_name,
+            seed=config.seed,
+            synthetic_class=ColoredSyntheticShift(
+                data_name=config.data_name, seed=config.seed
+            ),
+            version="stochastic",
+        )
+        train_dataset = ColoredMNIST(
+            root=os.path.join(config.data_path, "mnist"),
+            data_name=config.data_name,
+            split="train",
+            device=config.device,
+            data_shift_class=data_shift_class,
+        )
+        val_dataset = ColoredMNIST(
+            root=os.path.join(config.data_path, "mnist"),
+            data_name=config.data_name,
+            split="val",
+            device=config.device,
+            data_shift_class=data_shift_class,
+        )
+    else:
+        raise RuntimeError(f"Unknown dataset: {config.data_name}")
+    
+    return BaseLoader(train_dataset), BaseLoader(val_dataset)
 
 
 class AugMixDataset(torch.utils.data.Dataset):
--- pretrain/third_party/utils.py
+++ pretrain/third_party/utils.py
@@ -1,24 +1,136 @@
 # utils functions.
+import copy
 import numpy as np
+import sys
+sys.path.append("..")
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
+
+import ttab.model_adaptation.utils as adaptation_utils
+from ttab.loads.models import resnet, WideResNet
+from ttab.configs.datasets import dataset_defaults
 
 
-class LabelSmoothingCrossEntropy(nn.Module):
-    def __init__(self):
-        super(LabelSmoothingCrossEntropy, self).__init__()
+def convert_iabn(module: nn.Module, config, **kwargs) -> nn.Module:
+    module_output = module
+    if isinstance(module, nn.BatchNorm2d) or isinstance(module, nn.BatchNorm1d):
+        IABN = (
+            adaptation_utils.InstanceAwareBatchNorm2d
+            if isinstance(module, nn.BatchNorm2d)
+            else adaptation_utils.InstanceAwareBatchNorm1d
+        )
+        module_output = IABN(
+            num_channels=module.num_features,
+            k=config.iabn_k,
+            eps=module.eps,
+            momentum=module.momentum,
+            threshold=config.threshold_note,
+            affine=module.affine,
+        )
 
-    def forward(self, x, target, smoothing=0.1):
-        confidence = 1.0 - smoothing
-        logprobs = F.log_softmax(x, dim=-1)
-        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
-        nll_loss = nll_loss.squeeze(1)
-        smooth_loss = -logprobs.mean(dim=-1)
-        loss = confidence * nll_loss + smoothing * smooth_loss
-        return loss.mean()
+        module_output._bn = copy.deepcopy(module)
 
+    for name, child in module.named_children():
+        module_output.add_module(name, convert_iabn(child, **kwargs))
+    del module
+    return module_output
+
+def update_pytorch_model_key(state_dict: dict) -> dict:
+    """This function is used to modify the state dict key of pretrained model from pytorch."""
+    new_state_dict = {}
+    for key, value in state_dict.items():
+        if "downsample" in key:
+            name_split = key.split(".")
+            if name_split[-2] == "0":
+                name_split[-2] = "conv" 
+                new_key = ".".join(name_split)
+                new_state_dict[new_key] = value
+            elif name_split[-2] == "1":
+                name_split[-2] = "bn" 
+                new_key = ".".join(name_split)
+                new_state_dict[new_key] = value
+        elif "fc" in key:
+            name_split = key.split(".")
+            if name_split[0] == "fc":
+                name_split[0] = "classifier"
+                new_key = ".".join(name_split)
+                new_state_dict[new_key] = value
+        else:
+            new_state_dict[key] = value
+
+    return new_state_dict
+
+def build_model(config) -> nn.Module:
+    """Build model from `config`"""
+    num_classes = dataset_defaults[config.base_data_name]["statistics"]["n_classes"]
+    if config.base_data_name in ["officehome", "pacs", "waterbirds"]:
+        pretrained_model = models.resnet50(pretrained=True)
+        pretrained_model.fc = nn.Linear(pretrained_model.fc.in_features, out_features=num_classes, bias=False)
+        pretrained_model.fc.weight.data.normal_(mean=0, std=0.01)
+
+        model = resnet(config.base_data_name, depth=50, split_point=config.entry_of_shared_layers, grad_checkpoint=True).to(config.device)
+        model_dict = model.state_dict()
+        pretrained_dict = pretrained_model.state_dict()
+        new_pretrained_dict = update_pytorch_model_key(pretrained_dict)
+        new_pretrained_dict = {k: v for k, v in new_pretrained_dict.items() if k in model_dict}
+        model.load_state_dict(new_pretrained_dict)
+        model.to(config.device)
+        del pretrained_dict, pretrained_model, new_pretrained_dict
+        if config.use_iabn:
+            model = convert_iabn(model, config)
+    else:
+        if "wideresnet" in config.model_name:
+            components = config.model_name.split("_")
+            depth = int(components[0].replace("wideresnet", ""))
+            widen_factor = int(components[1])
+
+            model = WideResNet(
+                depth,
+                widen_factor,
+                num_classes,
+                split_point=config.entry_of_shared_layers,
+                dropout_rate=0.3,
+            )
+        elif "resnet" in config.model_name:
+            depth = int(config.model_name.replace("resnet", ""))
+            model = resnet(
+                config.base_data_name,
+                depth,
+                split_point=config.entry_of_shared_layers,
+                group_norm_num_groups=config.group_norm,
+            ).to(config.device)
+            if config.use_iabn:
+                assert config.group_norm is None, "IABN cannot be used with group norm."
+                model = convert_iabn(model, config)
+    return model
+
+def get_train_params(model: nn.Module, config) -> list:
+    """Define the trainable parameters for a model using `config`"""
+    if config.base_data_name in ["officehome", "pacs"]:
+        params = []
+        learning_rate = config.lr
+
+        for name_module, module in model.main_model.named_children():
+            if name_module != "classifier":
+                for _, param in module.named_parameters():
+                    params += [{"params": param, "lr": learning_rate*0.1}]
+            else:
+                for _, param in module.named_parameters():
+                    params += [{"params": param, "lr": learning_rate}]
+        
+        for name_module, module in model.ssh.head.named_children():
+            if isinstance(module, nn.Linear):
+                for _, param in module.named_parameters():
+                    params += [{"params": param, "lr": learning_rate}]
+            else:
+                for _, param in module.named_parameters():
+                    params += [{"params": param, "lr": learning_rate*0.1}]
+    else:
+        params = list(model.main_model.parameters()) + list(model.ssh.head.parameters())
+    return params
 
 def get_lr(step, total_steps, lr_max, lr_min):
     """Compute learning rate according to cosine annealing schedule."""
